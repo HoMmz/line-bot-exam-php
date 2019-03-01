147e02b1b7458a323c5176f7ac767ce76fc557e0
HoMmz <rapeevat_lp@hotmail.com>
2019-03-01 12:18:00 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = 'qbbNi2Wxf/s8BTGL9yR03jGJx4P2TbHWDZ1fNwchgMxXyh7Gs4ql2GdjQMwVXdU/PjS28u039uXb7rxLiIyQe4yRJw2bDULiTV0Glda24C43/EpsIFJKvj5TIYDQnH13CFmn73d/xQmQCj4I2LhB3QdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
 $content = file_get_contents('php://input');
